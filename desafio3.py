b99a2b2329d24916a111c377119ecf6d3f69eae6
vargasmachado-sketch <vargas.machado@escola.pr.gov.br>
2026-03-25 08:44:16 -0300

    Update desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -1,7 +1,12 @@
 nomes = []
+
 for i in range(5):
     nome = input(f"Digite o nome {i+1}: ")
     nomes.append(nome)
-print("Os nomes digitados foram:")
+
+print("Os nomes digitados são:")
+
 for nome in nomes:
-    print(nome)
+
+    print(f" - {nome}")
+
